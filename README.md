d3736a0e033871ef4c36eaa8af2ac4fb005c53ad
0xlarry <26318510+0xlarry@users.noreply.github.com>
2021-04-13 21:07:46 -0500

    Update

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # encrypt-keystore
 
-## Usage
+Convert mnemonic phrases to JSON keystore files compatible with [THORSwap](https://thorswap.finance/) and [Asgardex](https://app.asgard.exchange/).
+
+## How to use
+
+Clone this repository
+
+```bash
+git clone https://github.com/0xlarry/encrypt-keystore.git
+```
+
+Install [parcel](https://www.npmjs.com/package/parcel-bundler) and [ts-node](https://www.npmjs.com/package/ts-node).
+
+```bash
+npm install -g parcel-bundler ts-node
+```
+
+Now, **disconnect from internet** (important for your security!) then run
 
 ```bash
 parcel index.html
 ```
+
+The webapp should now be accessible at [http://localhost:1234](http://localhost:1234). Once you're done generating the keystore, hit `Ctrl + C` to exit.
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,59 @@
 import { validatePhrase, encryptToKeyStore } from "@xchainjs/xchain-crypto";
+import { delay } from "@xchainjs/xchain-util";
 
-let mnemonic = "";
-let password = "";
+/**
+ * @dev Forked from
+ * https://github.com/thorswap/thorswap-finance-app/blob/main/src/helpers/download.ts
+ */
+export const downloadAsFile = async (filename: string, text: string) => {
+  const element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`
+  );
+  element.setAttribute("download", filename);
 
-if (!validatePhrase(mnemonic)) {
-  console.log("Invalid mnemonic!");
-}
+  element.style.display = "none";
+  document.body.appendChild(element);
 
-if (password.length < 1) {
-  console.log("Invalid password!");
-}
+  element.click();
 
-encryptToKeyStore(mnemonic, password).then((keystore) => {
-  console.log(keystore);
+  document.body.removeChild(element);
+
+  await delay(100);
+};
+
+/**
+ * @dev Modified from
+ * https://github.com/thorswap/thorswap-finance-app/blob/main/src/components/WalletModal/CreateKeystore/Keystore.tsx
+ */
+$("#submitBtn").click(() => {
+  const mnemonic = $("#mnemonicInput").val();
+  const password = $("#passwordInput").val();
+
+  if (!mnemonic || !validatePhrase(mnemonic.toString())) {
+    $("#mnemonicInput").addClass("is-invalid");
+    return console.log("Invalid mnemonic:", mnemonic);
+  } else {
+    $("#mnemonicInput").removeClass("is-invalid");
+    console.log("Valid mnemonic:", mnemonic);
+  }
+
+  if (!password || password.toString().length < 1) {
+    $("#passwordInput").addClass("is-invalid");
+    return console.log("Invalid password:", password);
+  } else {
+    $("#passwordInput").removeClass("is-invalid");
+    console.log("Valid password:", password);
+  }
+
+  encryptToKeyStore(mnemonic.toString(), password.toString()).then(
+    (keystore) => {
+      downloadAsFile(
+        "asgardex-keystore.json",
+        JSON.stringify(keystore, null, 2)
+      );
+      console.log("Keystore generated:", keystore);
+    }
+  );
 });
